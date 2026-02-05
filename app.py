8b768b5a11226e5b3b58f9dcf28898bae5f3cf0f
Jmuhleman <71265526+Jmuhleman@users.noreply.github.com>
2026-02-05 16:11:44 +0100

    Fix typo in title for interview preparation app

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if load_dotenv:
 
 
 st.set_page_config(page_title="Coach en entretien d'embauche", layout="centered")
-st.title("Pose moi des questions sur les entretien pour une bonne préparation")
+st.title("Pose moi des questions sur l'entretien pour une bonne préparation")
 
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY", "")
 PINECONE_INDEX = os.getenv("PINECONE_INDEX", "test")
